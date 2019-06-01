e17b66dac203541b51b275198cd3bf7221749288
muril <muril@LAPTOP-C8P5BO14>
2019-06-01 04:44:50 -0300

    Alpha 0.1 // Ínicio

---
+++ ArvoresAVL/src/br/com/arvoreAVL/Leia.java
@@ -0,0 +1,60 @@
+package br.com.arvoreAVL;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Leia {
+	
+	//Bora cambada//
+
+  public static void main(String[] args) {
+
+    Leia obj = new Leia();
+    obj.run();
+
+  }
+
+  public void run() {
+
+    String arquivoCSV = "./arq/Professores.csv";
+    BufferedReader br = null;
+    String linha = "";
+    String csvDivisor = ",";
+    try {
+
+        br = new BufferedReader(new FileReader(arquivoCSV));
+        while ((linha = br.readLine()) != null ) {
+
+            String[] professores = linha.split(csvDivisor);
+
+            System.out.println(professores[professores.length-1]);
+
+        }
+
+    } catch (FileNotFoundException e) {
+    	
+        e.printStackTrace();
+        
+    } catch (IOException e) {
+    	
+        e.printStackTrace();
+        
+    } finally {
+    	
+        if (br != null) {
+        	
+            try {
+            	
+                br.close();
+                
+            } catch (IOException e) {
+            	
+                e.printStackTrace();
+            }
+        }
+    }
+  }
+
+}
