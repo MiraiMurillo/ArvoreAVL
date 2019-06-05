3375e6ed620e8a70f02eadffe1190602fe89d614
Dhionatã <xiyunagi@gmail.com>
2019-06-05 11:48:28 -0300

    Alpha 2.3 Criação da Classe Leia2, Problemas persiste.

--- ArvoresAVL/src/br/com/arvoreAVL/Leia.java
+++ ArvoresAVL/src/br/com/arvoreAVL/Leia.java
@@ -1,4 +1,5 @@
 package br.com.arvoreAVL;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,67 +10,52 @@ public class Leia {
 	private static String[] professores;
 
 	public static String[] profs(String arquivoCSV) {
-			
-			BufferedReader br = null;
-			String linha = "";
-			String csvDivisor = "  ";
-			setProfessores(null);
+
+		BufferedReader br = null;
+		String linha = "";
+		String csvDivisor = "  ";
+		setProfessores(null);
+
+		try {
+
+			br = new BufferedReader(new FileReader(arquivoCSV));
+			while ((linha = br.readLine()) != null) {
+
+				setProfessores(linha.split(csvDivisor));
+				return getProfessores();
+
+			}
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			if (br != null) {
 
 				try {
 
-					br = new BufferedReader(new FileReader(arquivoCSV));
-					while ((linha = br.readLine()) != null) {
-
-						setProfessores(linha.split(csvDivisor));
-						return getProfessores();
-
-					}
-
-				} catch (FileNotFoundException e) {
-
-					e.printStackTrace();
+					br.close();
 
 				} catch (IOException e) {
 
 					e.printStackTrace();
-
-				} finally {
-
-					if (br != null) {
-
-						try {
-
-							br.close();
-
-						} catch (IOException e) {
-
-							e.printStackTrace();
-						}
-					}
 				}
-				return null;
-			}
-
-			public static String[] getProfessores() {
-				return professores;
-			}
-
-			public static void setProfessores(String[] professores) {
-				Leia.professores = professores;
 			}
 		}
+		return null;
+	}
 
+	public static String[] getProfessores() {
+		return professores;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	public static void setProfessores(String[] professores) {
+		Leia.professores = professores;
+	}
+}
--- ArvoresAVL/src/br/com/arvoreAVL/Principal.java
+++ ArvoresAVL/src/br/com/arvoreAVL/Principal.java
@@ -2,23 +2,18 @@ package br.com.arvoreAVL;
 
 public class Principal {
 
-	
 	public static void main(String[] args) {
 
-		
-			String [] leitura = Leia.profs("./arq/professores.csv");
-			
-			
-			//testando vetor
-			for (int i = 0; i < leitura.length; i++) {
-				
-				System.out.println(leitura[i]);
-			}
-		
-			//testando saida
-			Saida.metodoPorScan(leitura);
-		
-		
-		
+		String[] leitura = Leia.profs("./arq/professores.csv");
+
+		// testando vetor
+		for (int i = 0; i < leitura.length; i++) {
+
+			System.out.println(leitura[i]);
 		}
+
+		// testando saida
+		Saida.metodoPorScan(leitura);
+
 	}
+}
